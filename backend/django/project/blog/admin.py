b8855a213563583fa5e17dca43cd5ab2dfe73212
Pavel <p.sharafiyev@arbuz.kz>
2023-09-30 22:15:44 +0600

    setup db

--- backend/django/project/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost, Comment, PostMeta, Tag
-# Register your models here.
-
-admin.site.register(BlogPost)
-admin.site.register(Comment)
-admin.site.register(PostMeta)
-admin.site.register(Tag)
